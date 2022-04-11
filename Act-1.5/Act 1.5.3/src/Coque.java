e15272650dd49e90fda00258c3aae593ebea69ae
Hamza0596 <103032210+Hamza0596@users.noreply.github.com>
2022-04-11 23:09:48 +0200

    Add files via upload

---
+++ Act-1.5/Act 1.5.3/src/Coque.java
@@ -0,0 +1,20 @@
+public class Coque {
+	public int longueur;
+	public int largeur;
+	private final int identif=2;
+	public Coque() {
+		this(15);
+	}
+	private Coque(int longueur) {
+		this.longueur=longueur;
+	}
+	public int indentifGetter () {
+		return this.identif;
+	}
+	public String toString () {
+		return ("La Coque N°"+this.identif+".");
+	}
+
+
+
+}
