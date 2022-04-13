a528b9524a68e4168b2bc2ef3ab6efe3f24d3ab0
Hamza0596 <103032210+Hamza0596@users.noreply.github.com>
2022-04-13 23:57:01 +0200

    Add files via upload

---
+++ 1.7/Micro proejt/src/OursAction.java
@@ -0,0 +1,10 @@
+
+public interface OursAction {
+	
+	public void Afficher_caractéristiques_ours() ;
+	public void Grimper ();
+	public void Creuser() ;
+	public void Hiverner() ;
+	public void Hurler () ; 
+
+}
---
+++ 1.7/Micro proejt/src/PredateurAction.java
@@ -0,0 +1,14 @@
+
+public interface PredateurAction {
+	
+    public void Se_nourrir ();
+	
+	public void Chasser ();
+	
+	public void Courrir() ;
+	
+	public void Se_reproduire ();
+	
+	public void Emettre_un_son();
+
+}
