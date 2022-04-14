c91d18dd0ecde6d84010ff11c78a11146091fa35
Hamza0596 <103032210+Hamza0596@users.noreply.github.com>
2022-04-14 16:34:01 +0200

    Add files via upload

---
+++ 1.8/1.8/src/GrpLionAction.java
@@ -0,0 +1,8 @@
+
+public interface GrpLionAction {
+	
+	public void constituer_couple_dominant ();
+	public void lancer_reproduction_lions() ;
+	public void déclarer_lions_non_dominant();
+
+}
---
+++ 1.8/1.8/src/OursAction.java
@@ -0,0 +1,9 @@
+
+public interface OursAction {
+
+	public void Afficher_caractéristiques_ours() ;
+	public void Grimper ();
+	public void Creuser() ;
+	public void Hiverner() ;
+	public void Hurler () ; 
+}
