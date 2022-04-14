1726a9d8969560f561c3c81fd075913bc3e3b497
Hamza0596 <103032210+Hamza0596@users.noreply.github.com>
2022-04-14 16:35:26 +0200

    Add files via upload

---
+++ 1.8/src/LionAction.java
@@ -0,0 +1,12 @@
+
+public interface LionAction {
+	
+	
+	
+	public void Affiche_caractéristiques();
+	public void Rugir();
+	public void Entendre_un_son();
+	public void séparer_temporairement(); 
+
+
+}
