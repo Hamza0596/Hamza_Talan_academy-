d3ff533067d31c820b132b70e32db59da1ab8f52
Hamza0596 <103032210+Hamza0596@users.noreply.github.com>
2022-04-14 22:32:44 +0200

    Delete 1.8 directory

--- 1.8/src/GrpLionAction.java
+++
@@ -1,8 +0,0 @@
-
-public interface GrpLionAction {
-	
-	public void constituer_couple_dominant (Lion l1,Lion L2);
-	public void lancer_reproduction_lions() ;
-	public void déclarer_lions_non_dominant();
-
-}
