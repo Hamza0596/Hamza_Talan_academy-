9131a599c78979182d20d53b5217612d494e4afd
Hamza0596 <103032210+Hamza0596@users.noreply.github.com>
2022-04-27 01:35:31 +0200

    Add files via upload

---
+++ 2.7/src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+
+	    // create a small dog
+	    Dog dog = DogFactory.getDog("small");
+	    dog.speak();
+
+	    // create a big dog
+	    dog = DogFactory.getDog("big");
+	    dog.speak();
+
+	    // create a working dog
+	    dog = DogFactory.getDog("working");
+	    dog.speak();
+		
+		
+		
+	}
+
+}
